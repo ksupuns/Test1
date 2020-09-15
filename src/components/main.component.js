a307185129adeff50c13be59669c760e923985f3
ksupuns <58617292+ksupuns@users.noreply.github.com>
2020-09-15 12:18:50 +0530

    Update main.component.js
    updated

--- src/components/main.component.js
+++ src/components/main.component.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
-import Axios from 'axios';
+import axios from 'axios';
 import { toast } from 'react-toastify';
 
 export default class main extends Component{
     constructor(props){
         super(props);
+
+        this.onchange_pid = this.onchange_pid.bind(this);
+        this.onchange_pname = this.onchange_pname.bind(this);
+        this.onchange_pnic = this.onchange_pnic.bind(this);
+        this.onchange_ptp = this.onchange_ptp.bind(this);
+        this.onsubmit = this.onsubmit.bind(this);
 
         this.state={
             p_id:'',
@@ -12,51 +18,53 @@ export default class main extends Component{
             p_nic:'',
             p_tp:''
         }
-
-        this.onchange_pid = this.onchange_pid.bind(this);
-        this.onchange_pname = this.onchange_pname.bind(this);
-        this.onchange_pnic = this.onchange_pnic.bind(this);
-        this.onchange_ptp = this.onchange_ptp.bind(this);
-
     }
 
     onchange_pid(e){
         this.setState({
             p_id:e.target.value
         });
-        console.log(this.state.p_id);
     }
     onchange_pname(e){
         this.setState({
             p_name:e.target.value
         });
-        console.log(this.state.p_name);        
     }
     onchange_pnic(e){
         this.setState({
             p_nic:e.target.value
         });
-        console.log(this.state.p_nic);        
     }
     onchange_ptp(e){
         this.setState({
             p_tp:e.target.value
-        });
-        console.log(this.state.p_tp);        
+        });     
     }
     onsubmit(e){
         e.preventDefault();
+        console.log(this.state.p_id);
+        console.log(this.state.p_name);
+        console.log(this.state.p_nic);
+        console.log(this.state.p_tp);
 
-        let submitobject={
+        const submitobject={
             pid:this.state.p_id,
             pname:this.state.p_name,
             pnic:this.state.p_nic,
             ptp:this.state.p_tp
         }
 
-        Axios.get('http://localhost:4000/add',submitobject)
-        .then(response=>{
-            toast(response.data);
+        // axios.get('http://localhost:4000/add',submitobject)
+        // .then(res => toast(res.data));
+
+        axios.post('http://localhost:4000/add',submitobject)
+        .then(res => toast(res.data));
+
+        this.setState({
+            p_id:'',
+            p_name:'',
+            p_nic:'',
+            p_tp:''
         });
     }        
 
@@ -71,31 +79,31 @@ export default class main extends Component{
                     <div className="row">
                         <div className="col-md-4"></div>
                         <div className="col-md-1">ID</div>
-                        <div className="col-md-5"><input type="text" value={this.p_id} onChange={this.onchange_pid}></input></div>
+                        <div className="col-md-5"><input type="text" value={this.state.p_id} onChange={this.onchange_pid}></input></div>
                         <div className="col-md-2"></div>
                     </div>
                     <div className="row">
                         <div className="col-md-4"></div>
                         <div className="col-md-1">Name</div>
-                        <div className="col-md-5"><input type="text" value={this.p_name} onChange={this.onchange_pname}></input></div>
+                        <div className="col-md-5"><input type="text" value={this.state.p_name} onChange={this.onchange_pname}></input></div>
                         <div className="col-md-2"></div>
                     </div>
                     <div className="row">
                         <div className="col-md-4"></div>
                         <div className="col-md-1">NIC</div>
-                        <div className="col-md-5"><input type="text" value={this.p_nic} onChange={this.onchange_pnic}></input></div>
+                        <div className="col-md-5"><input type="text" value={this.state.p_nic} onChange={this.onchange_pnic}></input></div>
                         <div className="col-md-2"></div>
                     </div>
                     <div className="row">
                         <div className="col-md-4"></div>
                         <div className="col-md-1">TP</div>
-                        <div className="col-md-5"><input type="text" value={this.p_tp} onChange={this.onchange_ptp}></input></div>
+                        <div className="col-md-5"><input type="text" value={this.state.p_tp} onChange={this.onchange_ptp}></input></div>
                         <div className="col-md-2"></div>
                     </div>                                                            
                     <div className="row">
                         <div className="col-md-4"></div>
                         <div className="col-md-1">TP</div>
-                        <div className="col-md-5"><input type="submit" value={this.p_tp} onClick={this.onsubmit}></input></div>
+                        <div className="col-md-5"><input type="submit" value = "Submit" onClick={this.onsubmit}></input></div>
                         <div className="col-md-2"></div>
                     </div>                 
             </div>
